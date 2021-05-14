37b3d018497f93539213045fec5f03633a060efe
Ayush P Gupta <ayushpguptaapg@gmail.com>
2021-05-14 19:40:53 +0530

    Initial setup
    Signed-off-by: Ayush P Gupta <ayushpguptaapg@gmail.com>

--- index.ts
+++ index.ts
@@ -1,12 +1,13 @@
 var createError = require('http-errors');
-var express = require('express');
+import express, { Request, Response } from 'express';
+
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-const {Provider} = require('oidc-provider');
+const { Provider } = require('oidc-provider');
 
 var app = express();
 
@@ -16,19 +17,20 @@ app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 const configuration = {
-    // ... see the available options in Configuration options section
-    clients: [{
-        client_id: 'foo',
-        client_secret: 'bar',
-        redirect_uris: ['http://lvh.me:8080/cb'],
-        // + other client properties
-    }],
-    // ...
+  // ... see the available options in Configuration options section
+  clients: [{
+    client_id: 'foo',
+    client_secret: 'bar',
+    redirect_uris: ['http://lvh.me:8080/cb'],
+    // + other client properties
+  }],
+
+  // ...
 };
 const oidc = new Provider('http://localhost:3000', configuration);
 app.use('/oidc', oidc.callback());
@@ -38,19 +40,19 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
-    next(createError(404));
+app.use((req, res, next) => {
+  next(createError(404));
 });
 
 // error handler
-app.use(function (err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use((err: any, req: Request, res: Response, next: any) => {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-    // render the error page
-    res.status(err.status || 500);
-    res.render('error');
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
 });
 
 module.exports = app;
