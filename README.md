861acd0c01fe9c3bba42ac25ddbbfccf61c3f115
Ayush P Gupta <ayushpguptaapg@gmail.com>
2021-05-14 09:43:11 +0530

    Initial Commit
    Signed-off-by: Ayush P Gupta <ayushpguptaapg@gmail.com>

---
+++ README.md
@@ -0,0 +1,14 @@
+# FA OpenId Connect Provider
+
+A sample demonstrating OpenId Connect Provider built on NodeJs
+
+## Packages
+
+This sample is powered by [oidc-provider](https://www.npmjs.com/package/oidc-provider) NPM package
+
+## Getting Started
+
+1. Run `npm i`
+2. Run `npm run start`
+3. Open [http://localhost:3000/oidc/.well-known/openid-configuration](http://localhost:3000/oidc/.well-known/openid-configuration) for seeing configuration.
+
---
+++ app.js
@@ -0,0 +1,56 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+const {Provider} = require('oidc-provider');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+const configuration = {
+    // ... see the available options in Configuration options section
+    clients: [{
+        client_id: 'foo',
+        client_secret: 'bar',
+        redirect_uris: ['http://lvh.me:8080/cb'],
+        // + other client properties
+    }],
+    // ...
+};
+const oidc = new Provider('http://localhost:3000', configuration);
+app.use('/oidc', oidc.callback());
+
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+});
+
+module.exports = app;
