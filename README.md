0c70ec66d0baa0caee8b04aa71faa4781197f367
Ayush P Gupta <ayushpguptaapg@gmail.com>
2021-05-15 20:49:53 +0530

    Add custom ui
    Signed-off-by: Ayush P Gupta <ayushpguptaapg@gmail.com>

--- README.md
+++ README.md
@@ -12,3 +12,6 @@ This sample is powered by [oidc-provider](https://www.npmjs.com/package/oidc-pro
 2. Run `npm run start`
 3. Open [http://localhost:3000/oidc/.well-known/openid-configuration](http://localhost:3000/oidc/.well-known/openid-configuration) for seeing configuration.
 
+## Further Reading
+
+1. [scottbrady91.com/OpenID-Connect/Getting-Started-with-oidc-provider](https://www.scottbrady91.com/OpenID-Connect/Getting-Started-with-oidc-provider)
--- index.ts
+++ index.ts
@@ -1,43 +1,136 @@
-var createError = require('http-errors');
-import express, { Request, Response } from 'express';
+const createError = require("http-errors");
+import express, { Request, Response, urlencoded } from "express";
+import { Provider } from "oidc-provider";
 
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-const { Provider } = require('oidc-provider');
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
+const body = urlencoded({ extended: false });
 
-var app = express();
+const app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set("views", path.join(__dirname, "views"));
+// app.set('view engine', 'pug');
+app.set("view engine", "ejs");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-const configuration = {
-  // ... see the available options in Configuration options section
-  clients: [{
-    client_id: 'foo',
-    client_secret: 'bar',
-    redirect_uris: ['http://lvh.me:8080/cb'],
-    // + other client properties
-  }],
+const oidc = new Provider("http://localhost:3000", {
+  clients: [
+    {
+      client_id: "foo",
+      client_secret: "bar",
+      redirect_uris: ["https://azure.fieldassist.io/"],
+      grant_types: ["implicit"],
+      response_types: ["id_token"],
+      token_endpoint_auth_method: "none",
+    },
+  ],
+  responseTypes: ["id_token"],
+  scopes: ["openid"],
+  features: {
+    devInteractions: {
+      enabled: false,
+    },
+  },
+  async loadExistingGrant(ctx) {
+    const grantId =
+      (ctx?.oidc?.result &&
+        ctx?.oidc?.result?.consent &&
+        ctx?.oidc?.result?.consent?.grantId) ||
+      ctx?.oidc?.session?.grantIdFor(ctx?.oidc?.client?.clientId!);
 
-  // ...
-};
-const oidc = new Provider('http://localhost:3000', configuration);
-app.use('/oidc', oidc.callback());
+    if (grantId) {
+      return ctx?.oidc?.provider?.Grant.find(grantId);
+    } else {
+      const grant = new ctx.oidc.provider.Grant();
+      (grant.clientId = ctx?.oidc?.client?.clientId),
+        (grant.accountId = ctx?.oidc?.session?.accountId),
+        grant.addOIDCScope("openid email profile");
+      grant.addOIDCClaims(["first_name"]);
+      grant.addResourceScope(
+        "urn:example:resource-indicator",
+        "api:read api:write"
+      );
+      await grant.save();
+      return grant;
+    }
+  },
+});
 
+app.use("/oidc", oidc.callback());
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+function setNoCache(req: Request, res: Response, next: any) {
+  res.set("Pragma", "no-cache");
+  res.set("Cache-Control", "no-cache, no-store");
+  next();
+}
+
+app.get("/interaction/:uid", async (req, res, next) => {
+  try {
+    const { uid, prompt, params, session } = await oidc.interactionDetails(
+      req,
+      res
+    );
+
+    const id: unknown = params.client_id;
+    const client11 = await oidc.Client.find(typeof id === "string" ? id : "12");
+    switch (prompt.name) {
+      case "login": {
+        return res.render("login", {
+          client11,
+          uid,
+          details: prompt.details,
+          params,
+          title: "Sign-in",
+          //session: session ? debug(session) : undefined,
+          // dbg: {
+          //   params: debug(params),
+          //   prompt: debug(prompt),
+          // },
+        });
+      }
+      default:
+        return undefined;
+    }
+  } catch (err) {
+    return next(err);
+  }
+});
+
+app.post(
+  "/interaction/:uid/login",
+  setNoCache,
+  body,
+  async (req, res, next) => {
+    try {
+      const {
+        prompt: { name },
+      } = await oidc.interactionDetails(req, res);
+
+      const result = {
+        login: {
+          accountId: "12345",
+        },
+      };
+
+      await oidc.interactionFinished(req, res, result, {
+        mergeWithLastSubmission: false,
+      });
+    } catch (err) {
+      console.error(err);
+      next(err);
+    }
+  }
+);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
@@ -48,11 +141,15 @@ app.use((req, res, next) => {
 app.use((err: any, req: Request, res: Response, next: any) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
-module.exports = app;
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`The application is listening on port ${port}!`);
+});
